96c87320d0d701fac460e3cbd45448118e4a7026
Rinaldi <rinaldi.guizot@dataon.com>
2018-05-12 14:32:23 +0700

    User Model dll

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://aldi:aldi@ds119490.mlab.com:19490/aldiconnector"
+  mongoURI: "mongodb://aldi:aldi@ds119490.mlab.com:19490/aldiconnector",
+  // mongoURI: "mongodb://localhost/devconnector"
+  secretOrKey: 'secret'
 };
